ab490ec59c01295ffa967b57697db15111bfbe84
Your Name <name@example.com>
2017-01-03 16:02:08 +0700

    done selection and bubble sort

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,33 +1,48 @@
 'use strict'
 
-function bubbleSort(items) {}
+function bubbleSort(items) {
+    var panjang = items.length;
+    for (var i = 0; i < panjang; i++) {
+        var last = panjang - (i + 1) //terakhir
+        var data1 = 0;
+        for (var j = 1; j < panjang; j++) {
+            if (items[last - j] > items[last]) {
+                data1 = items[last - j]
+                items[last - j] = items[last]
+                items[last] = data1
+            } else {
+                last = items[last - j]
+            }
+        }
+    }
+    return items;
+}
 
 
 // Driver code
 function main() {
-
-  sort_from_file('random_wordlist.txt')
-  sort_from_file('reversed_wordlist.txt')
-  sort_from_file('sorted_wordlist.txt')
+    sort_from_file('test_random_wordlist.txt')
+    sort_from_file('test_reversed_wordlist.txt')
+    //Aktifkan 3 Baris dibawah ini untuk mengaktifkan proses bubble sort untuk data lebih besar
+    // sort_from_file('random_wordlist.txt')//Menjalankan Fungsi sort_from_file() dengan data param random_wordlist.txt
+    // sort_from_file('reversed_wordlist.txt')//Menjalankan Fungsi sort_from_file() dengan data param reversed_wordlist.txt
+    // sort_from_file('sorted_wordlist.txt')//Menjalankan Fungsi sort_from_file() dengan data param sort_from_file.txt
 
 }
 
 function sort_from_file(filename) {
-  let fs = require('fs')
+    let fs = require('fs')
+    fs.readFile(filename, (err, data) => {
+        if (err)
+            return console.log(err)
 
-  fs.readFile(filename, (err, data) => {
+        let items = data.toString()
+            .split("\r\n")
+        console.log(filename)
+        console.log("--------")
+        console.log(bubbleSort(items))
 
-    if (err)
-      return console.log(err)
-
-    let items = data.toString()
-      .split("\r\n")
-
-    console.log(filename)
-    console.log("--------")
-    console.log(bubbleSort2(items))
-
-  })
+    })
 }
 
 main()
--- selection_sort.js
+++ selection_sort.js
@@ -1,32 +1,41 @@
 'use strict'
 
-function selectionSort(arrays){
+function selectionSort(items) {
+    var panjang = items.length;
+    for (var i = 0; i < panjang; i++) {
+        var min = i; //nilai minimal dalam array
+        var data1 = items[i];
+        for (var j = i + 1; j < panjang; j++) {
+            if (items[min] > items[j]) {
+                min = j
+            }
+        }
+        items[i] = items[min]
+        items[min] = data1
+    }
+    return items;
 }
 
 // Driver code
 function main() {
-
-  sort_from_file('random_wordlist.txt')
-  sort_from_file('reversed_wordlist.txt')
-  sort_from_file('sorted_wordlist.txt')
-
+    sort_from_file('test_random_wordlist.txt')
+    sort_from_file('test_reversed_wordlist.txt')
+    //Aktifkan 3 Baris dibawah ini untuk mengaktifkan proses selection sort untuk data lebih besar
+    // sort_from_file('random_wordlist.txt')//Menjalankan Fungsi sort_from_file() dengan data param random_wordlist.txt
+    // sort_from_file('reversed_wordlist.txt')//Menjalankan Fungsi sort_from_file() dengan data param reversed_wordlist.txt
+    // sort_from_file('sorted_wordlist.txt')//Menjalankan Fungsi sort_from_file() dengan data param sort_from_file.txt
 }
 
 function sort_from_file(filename) {
-  let fs = require('fs')
-
-  fs.readFile(filename, (err, data) => {
-
-    if (err)
-      return console.log(err)
-
-    let items = data.toString().split("\r\n")
-
-    console.log(filename)
-    console.log("--------")
-    console.log(selectionSort(items))
-
-  })
+    let fs = require('fs')
+    fs.readFile(filename, (err, data) => {
+        if (err)
+        return console.log(err)
+        let arr = data.toString().trim().split("\r\n")
+        console.log(filename)
+        console.log("--------")
+        console.log(selectionSort(arr)) //Menjalankan selectionSort() dengan data param
+    })
 }
 
-main()
+main() //Menjalankan fungsi main()/
